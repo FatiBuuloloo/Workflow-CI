058570020a816de71df90c8d9c9a8f0c10d480f3
Fati Buulolo <fatibuulolo2@yahoo.com>
2026-05-28 01:52:47 +0700

    refactor: remove inline tracking uri to fix mlflow run collision

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -4,13 +4,9 @@ import pandas as pd
 import mlflow
 import mlflow.sklearn
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import PowerTransformer
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.ensemble import RandomForestRegressor
 
-base_dir = os.path.dirname(os.path.abspath(__file__))
-mlruns_dir = os.path.join(base_dir, "mlruns")
-mlflow.set_tracking_uri(f"file://{mlruns_dir}")
 mlflow.sklearn.autolog()
 data_path = "calories_processed.csv" 
 if not os.path.exists(data_path):
@@ -18,6 +14,7 @@ if not os.path.exists(data_path):
 data = pd.read_csv(data_path)
 if "User_ID" in data.columns:
     data.drop("User_ID", axis=1, inplace=True)
+
 X = data.drop("Calories", axis=1)
 y = data["Calories"]
 
@@ -27,10 +24,9 @@ with mlflow.start_run():
     model.fit(train_X, train_y)
     mlflow.sklearn.log_model(model, "model")
     y_pred = model.predict(test_X)
-
     mse = mean_squared_error(test_y, y_pred)
     mae = mean_absolute_error(test_y, y_pred)
     rmse = np.sqrt(mse)
-
     print(f"Hasil Evaluasi -> MAE: {mae:.4f}, MSE: {mse:.4f}, RMSE: {rmse:.4f}")
+
 print("Training Selesai! MLflow Projects berhasil merekam model steril Anda.")
